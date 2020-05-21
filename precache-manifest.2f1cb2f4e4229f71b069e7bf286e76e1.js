cf323340d30e0d5b47bb7783dcf646e647a5c52c
Vinicius Gerevini <viniciusgerevini@gmail.com>
2020-05-22 00:01:55 +1000

    deploy first version

---
+++ precache-manifest.2f1cb2f4e4229f71b069e7bf286e76e1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e99b58a7b7f2cf066a887e817e32da1d",
+    "url": "/react-responsive-mosaic/index.html"
+  },
+  {
+    "revision": "b16cc46ec221bb50b6df",
+    "url": "/react-responsive-mosaic/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "91cba646f7a12414da4c",
+    "url": "/react-responsive-mosaic/static/js/2.9c42581c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-responsive-mosaic/static/js/2.9c42581c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b16cc46ec221bb50b6df",
+    "url": "/react-responsive-mosaic/static/js/main.ac9319ee.chunk.js"
+  },
+  {
+    "revision": "e4c692381ea580a42cf9",
+    "url": "/react-responsive-mosaic/static/js/runtime-main.c10d08f1.js"
+  }
+]);
